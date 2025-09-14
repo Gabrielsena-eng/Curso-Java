dbe3e3893669e9d5d9bf8ec179ddea15b493c7f9
Antonio Gabriel Sena De albuquerque <gabrielsena10072@gmail.com>
2025-09-14 10:50:51 -0300

    Exercicios-Novos

---
+++ Estudos/src/Exercicios/CalculoArea.java
@@ -0,0 +1,36 @@
+package Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalculoArea {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        double A, B, C;
+        double areaTrianguloRetangulo, areaCirculo, areaTrapezio, areaQuadrado, areaRetangulo;
+        Scanner sc = new Scanner(System.in);
+
+        Locale.setDefault(Locale.US);
+        A = sc.nextDouble();
+        sc.nextLine();
+        B = sc.nextDouble();
+        sc.nextLine();
+        C = sc.nextDouble();
+        sc.nextLine();
+        sc.close();
+
+        areaTrianguloRetangulo = (A * C)/ 2;
+        areaCirculo = (3.14159 * Math.pow(C, 2.0));
+        areaTrapezio = ((A + B) * C) / 2;
+        areaQuadrado = B * B;
+        areaRetangulo = A * B;
+
+        System.out.printf("TRIANGULO: %.3f.%n ", areaTrianguloRetangulo);
+        System.out.printf("CIRCULO: %.3f.%n", areaCirculo);
+        System.out.println("TRAPEZIO:" + areaTrapezio);
+        System.out.println("QUADRADO:" + areaQuadrado);
+        System.out.println("RETANGULO:" + areaRetangulo);
+
+
+    }
+}
